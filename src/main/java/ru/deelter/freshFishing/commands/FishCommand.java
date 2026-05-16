eb285e758dcd2a9c3016f442b201929f6b5cfc8d
Roman <roma200315@mail.ru>
2026-05-16 20:25:30 +0300

    Remove message

--- src/main/java/ru/deelter/freshFishing/commands/FishCommand.java
+++ src/main/java/ru/deelter/freshFishing/commands/FishCommand.java
@@ -12,33 +12,32 @@ import ru.deelter.freshFishing.shop.FishSaleMenu;
 
 public class FishCommand implements CommandExecutor {
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String @NonNull [] args) {
-        // /fish menu
-        if (args.length == 1 && args[0].equalsIgnoreCase("menu")) {
-            if (sender instanceof Player player) {
-                player.openInventory(new FishSaleMenu().getInventory());
-                return true;
-            } else {
-                sender.sendMessage(Component.text("This command can only be executed by a player.", NamedTextColor.RED));
-                return true;
-            }
-        }
-        if (args.length == 2 && args[0].equalsIgnoreCase("menu")) {
-            Player target = Bukkit.getPlayer(args[1]);
-            if (target == null) {
-                sender.sendMessage(Component.text("Player not found: " + args[1], NamedTextColor.RED));
-                return true;
-            }
-            target.openInventory(new FishSaleMenu().getInventory());
-            
-            if (!sender.equals(target)) {
-                sender.sendMessage(Component.text("Opened fish shop for " + target.getName(), NamedTextColor.GREEN));
-                target.sendMessage(Component.text("Fish shop opened by " + sender.getName(), NamedTextColor.YELLOW));
-            }
-            return true;
-        }
-        sender.sendMessage(Component.text("Usage: /fish menu [player]", NamedTextColor.GRAY));
-        return true;
-    }
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String @NonNull [] args) {
+		// /fish menu
+		if (args.length == 1 && args[0].equalsIgnoreCase("menu")) {
+			if (sender instanceof Player player) {
+				player.openInventory(new FishSaleMenu().getInventory());
+				return true;
+			} else {
+				sender.sendMessage(Component.text("This command can only be executed by a player.", NamedTextColor.RED));
+				return true;
+			}
+		}
+		if (args.length == 2 && args[0].equalsIgnoreCase("menu")) {
+			Player target = Bukkit.getPlayer(args[1]);
+			if (target == null) {
+				sender.sendMessage(Component.text("Player not found: " + args[1], NamedTextColor.RED));
+				return true;
+			}
+			target.openInventory(new FishSaleMenu().getInventory());
+
+			if (!sender.equals(target)) {
+				sender.sendMessage(Component.text("Opened fish shop for " + target.getName(), NamedTextColor.GREEN));
+			}
+			return true;
+		}
+		sender.sendMessage(Component.text("Usage: /fish menu [player]", NamedTextColor.GRAY));
+		return true;
+	}
 }
